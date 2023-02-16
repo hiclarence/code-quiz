13c301d4d186a131c432fea2653bd35f3850f76d
clarence_c <clarence@biteline.net>
2023-02-15 23:10:17 -0800

    high score to load from local storage

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const highScoreArray = JSON.parse(localStorage.getItem("highScores")) || [];
 //identifies html structure including title
 var container = document.querySelector(".container");
 var questions = document.querySelector(".questions");
+var response = document.querySelector(".response");
 var innerSection = document.querySelector(".inner");
 var pTag = document.querySelector("p");
 var titleElement = document.querySelector(".welcome");
@@ -18,20 +19,58 @@ start.addEventListener("click", setTime);
 start.setAttribute("style", "padding: 20px; width: 10%; color: white; margin: 15px auto; border-radius: 5px; border: none; cursor: pointer; font-family; Arial; font-size: 16px;");
 
 //targeting score link
-score.addEventListener("click", viewScore);
 score.setAttribute("style", "cursor: pointer; text-decoration: none; color: purple");
+score.addEventListener("click", function() {
+    if (highScoreArray.length == 0) {
+        alert("You need to play a game first!")
+    } else {
+        viewScore();
+    }
+});
 
 var listElement = document.createElement("ul");
+// console.log(highScoreArray.length);
 
 var points = 0;
+
+let firstQuestion = {
+    prompt: "How do you write 'hello world?",
+    choices: ['console.log("Hello World")' , 
+    'print "hello world!', 
+    '"hello world!"'],
+    answer: 'console.log("Hello World")',
+};
+
+let secondQuestion = {
+    prompt: "Which script do we put in the HTML to reference Javascript?",
+    choices: ["scripting" , 
+    'js', 
+    'script.js'],
+    answer: 'script.js',
+};
+
+let thirdQuestion = {
+    prompt: "What is the correct place to put javascript?",
+    choices: ["the body section" , 
+    'the head section', 
+    'both are valid'],
+    answer: 'both are valid',
+};
+
+let questionBank = {
+   questionOne : firstQuestion,
+   questionTwo : secondQuestion,
+   questionThree : thirdQuestion
+}; 
 
 questions.setAttribute("style", "display:none;");
 
 //setting how many seconds to run
-var secondsLeft = 15;
+var secondsLeft = 10;
 
 function setTime() {
     startQuiz();
+    secondQuestionAttempt;
     // Sets interval in variable
 
     var timerInterval = setInterval(function() {
@@ -47,154 +86,89 @@ function setTime() {
     }, 1000);
 }
 
-let firstQuestion = {
-    prompt: "How do you write 'hello world?",
-    choices: ['console.log("Hello World")' , 
-    'print "hello world!', 
-    '"hello world!"'],
-    answer: 'console.log("Hello World")',
-};
-
-
-let secondQuestion = {
-    prompt: "'Which script do we put in the HTML to reference Javascript?'",
-    choices: ["scripting" , 
-    'js', 
-    'script.js'],
-    answer: 'script.js',
-};
-
-let thirdQuestion = {
-    prompt: "'What is the correct place to put javascript?'",
-    choices: ["the body section" , 
-    'the head section', 
-    'both are valid'],
-    answer: 'both are valid',
-};
-
-let questionBank = {
-   questionOne : firstQuestion,
-   questionTwo : secondQuestion,
-   questionThree : thirdQuestion
-}; 
 
 function startQuiz() {
+    innerSection.textContent = " ";
     pTag.textContent = " ";
     start.setAttribute("style", "display:none;");
-
     titleElement.setAttribute("style", "background-color: white");
-    //value returns all the questions 
-    // let value = Object.values(questionBank);
-
-    // for (var i = 0; i<3; i++) {
-    //     //value[i] = the question
-    //     console.log(value[i].prompt);
-    //     titleElement.textContent = value[i].prompt;    
-        
-    //     //setting the li elements to the answer choices possible
-    //     questions.children[i].textContent = value[0].choices[i];
-    //     // console.log(value[i].choices[i]); 
-    //     questions.setAttribute("style", "display:block; background-color: white;");
-    //     let choiceSelection = questions.children[i].textContent;
-    //     var responseText = document.createElement("h2");
-    
-    //     questions.children[i].addEventListener("click", function() {
-    //         if (choiceSelection === value[0].answer) {
-    //             points += 10;
-    //             responseText.textContent = "Correct!";
-    //             innerSection.appendChild(responseText);
-    //         } else {
-    //             secondsLeft -= 3;
-    //             responseText.textContent = "Wrong";
-    //             innerSection.appendChild(responseText);
-    //     }});
-        
-    // }
-
-
-    
-    // for (const question in questionBank) {
-    //     for (var i = 0; i<3; i++) {
-    //         console.log(value[0].prompt);
-    //         titleElement.textContent = value[0].prompt;    
-    //         questions.children[i].textContent = value[0].choices[i];
-    //         console.log(value[0].choices[i]);
-    //         questions.setAttribute("style", "display:block; background-color: white;");
-
-    //     }
-    //     for (var i = 0; i<3; i++) {
-    //         let val = questionBank[keys[i]]; 
-    //         console.log(val);
-    //         titleElement.textContent = question.value.prompt;    
-    //     }
-    // }
-    
     questions.setAttribute("style", "display:block; background-color: white;");
+    
+    //sets the title and questions
+    titleElement.textContent = firstQuestion.prompt;
+    var responseText = document.createElement("h2");
 
     //first question
     for (var i = 0; i<3; i++) {
-        console.log("first question hit!")
-        
-        //sets the title and questions
-        titleElement.textContent = firstQuestion.prompt;        
         questions.children[i].textContent = firstQuestion.choices[i];
-        
-        let choiceSelection = questions.children[i].textContent;
-        var responseText = document.createElement("h2");
-        questions.children[i].addEventListener("click", function() {
-            if (choiceSelection === firstQuestion.answer) {
-                points += 10;
-                responseText.textContent = "Correct!";
-                innerSection.appendChild(responseText);
-            } else {
-                secondsLeft -= 3;
-                responseText.textContent = "Wrong";
-                innerSection.appendChild(responseText);
-        }});
-    };
-
-    // titleElement.textContent = " ";
-    // questions.children.textContent = " "; 
-
-    //second question
-    // for (var i = 0; i<3; i++) {
-    //     titleElement.textContent = secondQuestion.prompt;        
-    //     questions.children[i].textContent = secondQuestion.choices[i];
     //     let choiceSelection = questions.children[i].textContent;
-    //     var responseText = document.createElement("h2");
     //     questions.children[i].addEventListener("click", function() {
-    //         if (choiceSelection === secondQuestion.answer) {
+    //         if (choiceSelection === firstQuestion.answer) {
     //             points += 10;
     //             responseText.textContent = "Correct!";
-    //             innerSection.appendChild(responseText);
     //         } else {
     //             secondsLeft -= 3;
-    //             responseText.textContent = "Wrong";
-    //             innerSection.appendChild(responseText);
-    //     }});
-    // };
-
+    //             responseText.textContent = "Wrong...";
+    //             console.log("wrong text printed")
+    //             }; 
+    // }); 
 };
 
-function secondQuestionTry() {
-    console.log("second question")
+    questions.children[1].addEventListener("click", function() {
+        secondsLeft -= 3; 
+        responseText.textContent = "Wrong1";
+        response.appendChild(responseText);
+    }); 
+
+    questions.children[2].addEventListener("click", function() {
+        secondsLeft -= 3; 
+        responseText.textContent = "Wrong2";
+        response.appendChild(responseText);
+    });
+
+    questions.children[0].addEventListener("click", function() {
+        points += 10; 
+        responseText.textContent = "Correct...";
+        response.appendChild(responseText);
+        secondQuestionAttempt();
+    });
+
+    console.log("end of button")
+};
+
+function secondQuestionAttempt() {
+    console.log("second question hit")
+    pTag.textContent = " ";
+    // response.textContent = " ";
+    titleElement.textContent = " ";
+
+    titleElement.textContent = secondQuestion.prompt;      
+    var responseText = document.createElement("h2");
+
     for (var i = 0; i<3; i++) {
-        titleElement.textContent = secondQuestion.prompt;        
         questions.children[i].textContent = secondQuestion.choices[i];
-        let choiceSelection = questions.children[i].textContent;
-        var responseText = document.createElement("h2");
-        questions.children[i].addEventListener("click", function() {
-            if (choiceSelection === secondQuestion.answer) {
-                points += 10;
-                responseText.textContent = "Correct!";
-                innerSection.appendChild(responseText);
-            } else {
-                secondsLeft -= 3;
-                responseText.textContent = "Wrong";
-                innerSection.appendChild(responseText);
-        }});
     };
+
+    questions.children[1].addEventListener("click", function() {
+        secondsLeft -= 3; 
+        responseText.textContent = "Wrong3";
+    }); 
+
+    questions.children[2].addEventListener("click", function() {
+        points += 10; 
+        responseText.textContent = "Correct2";
+    });
+
+    questions.children[0].addEventListener("click", function() {
+        secondsLeft -= 3; 
+        responseText.textContent = "Wrong4";
+        secondQuestionAttempt();
+    });
+
 }
+
+
+
 
 function points() {
     points += 10;
@@ -207,7 +181,7 @@ function points() {
 function sendMessage() {
     innerSection.textContent = " ";
     questions.setAttribute("style", "display:none;");
-
+    response.remove();
     titleElement.textContent = "All done!";
 
     //creates "final score is" text
@@ -227,7 +201,7 @@ function sendMessage() {
     aLabel.setAttribute("style", "background-color: white; font-family: Arial; font-size: 20px")
     aForm.appendChild(aLabel);
 
-    //creating the input
+    //creating the input box
     var formInput = document.createElement("input");
     formInput.setAttribute("style", "height: 30px")
     formInput.id = "name";
@@ -245,47 +219,74 @@ function sendMessage() {
 }
 
 function viewScore() {
-    var userName = document.getElementById("name").value;
 
     // clears the screen
     pTag.textContent = " ";
     innerSection.textContent = " ";
     start.setAttribute("style", "display:none;");
     timeElement.setAttribute("style", "display:none;");
-    score.setAttribute("style", "display:none;");
+    // score.setAttribute("style", "display:none;");
 
     //creates high score text
     titleElement.textContent = "High Score";
     titleElement.setAttribute("style", "background-color: white;");
 
-    //creates a name
-    var nameBoard = document.createElement("p");
-    nameBoard.textContent =  userName.concat(" - ", points);
-    nameBoard.setAttribute("style", "padding: 10px 10px; margin: 10px auto; background-color: #d6cefb; color: black; border: none; text-align: left")
+    //creates the name board section
+    var nameBoard = document.createElement("section");
     titleElement.appendChild(nameBoard);
+    nameBoard.setAttribute("style", "background-color: white;")
 
-    //inputs name and points into local storage    
-    let user = {
-        name : userName,
-        points : points
+
+    //creates and adds the new entry for the name board
+    // var entry = document.createElement("p");
+    // entry.textContent =  userName.concat(" - ", points);
+    // nameBoard.appendChild(entry);
+
+    //inputs name and points into local storage for a new entry
+    // var userName = document.getElementById("name").value;
+    // userEntry = {
+    //     name : userName,
+    //     points : points
+    // };
+    
+    // highScoreArray.push(userEntry);
+    // localStorage.setItem("highScores", JSON.stringify(highScoreArray));
+
+
+
+    //imports from local storage
+    if (highScoreArray.length != 0) {
+        for (var i=0 ; i<highScoreArray.length; i++) {
+            var entry = document.createElement("p");
+            var name = JSON.stringify(highScoreArray[i].name);
+            var score = JSON.stringify(highScoreArray[i].points);
+            entry.textContent =  name.concat(" - ", score );
+            entry.setAttribute("style", "padding: 10px 10px; margin: 10px auto; background-color: #d6cefb; color: black; border: none; text-align: left")
+            nameBoard.appendChild(entry);
+                      
+        }
     }
-
-    localStorage.setItem("user", JSON.stringify(user));
 
     //creates go back button
     var backButton = document.createElement("button");
     backButton.textContent = "Go Back";
     backButton.setAttribute("style", "padding: 10px; background-color: #9F2B68; width: 20%; color: white; margin: 5px auto; border-radius: 5px; font-family: Arial; font-size: 16px; cursor: pointer; border: none");
+    backButton.addEventListener("click", function() { window.location.reload();});  
 
     //creates clear score button
     var clearScore = document.createElement("button");
     clearScore.textContent = "Clear Score";
     clearScore.setAttribute("style", "padding: 10px; background-color: #9F2B68; width: 20%; color: white; margin: 5px auto; border-radius: 5px; font-family: Arial; font-size: 16px; cursor: pointer; border: none");
+    clearScore.addEventListener("click", function() {
+        localStorage.clear()
+    });
 
     //appends and styles both buttons
     innerSection.appendChild(backButton);
     innerSection.appendChild(clearScore);
     innerSection.setAttribute("style", "display: flex; flex-direction: row; width: 50%; margin: 20px auto")
     //**how do you reset back to original state when go back or clear is clicked?
+    
+
     
 }
